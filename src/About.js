0ff8cc9bb4bd5d06969c0e5aa4c415531b178ce4
anuraj07 <deep.anuraj10@gmail.com>
2020-05-28 00:30:33 +0530

    Still in progress

--- src/About.js
+++ src/About.js
@@ -3,8 +3,49 @@ import './Aboutstyle.css'
 
 const About = () => {
     return(
-        <div>
-            
+        <div className = "main">
+            <div className="box">
+            <h5>About Me</h5>
+            <div className = "flex-container">
+                <div className = "left_part">
+                <div className = "inner_left_part">
+                <div className = "pic"></div>
+                <div className = "details">
+                <b>Name: </b> Anuraj Deep<br></br><br></br>
+                <b>Profile: </b> CSE Student<br></br><br></br>
+                <b>Email: </b> deep.anuraj10@gmail.com<br></br><br></br>
+                <b>Phone: </b> +91 7903811835<br></br><br></br>
+                </div></div>
+
+                <p><b>Skills</b></p>
+
+                </div>
+                <div className = "right_part">
+                <p>Hi, Welcome to my cumulative Portfolio!
+                 My name is Ashutosh Ranjan (b. Feb 1998),
+                  a native-born Bihari, grew up in 
+                  Samastipur, India and now living in 
+                  Bengaluru, India.</p><br></br><br></br>
+
+                  <p>I'm an undergraduate furthermore pursuing 
+                my major in Computer Science & Engineering 
+                from Visweswaraya Technological University 
+                (VTU), Bengaluru. I had completed my early 
+                stage of education from my native state. I 
+                love to contribute my time to brush up my 
+                coding skills on HackerRank and Geeks for 
+                Geeks platforms.</p><br></br><br></br>
+
+                <p>I love watching Football and a huge fan of 
+                Cristiano Ronaldo. Real Madrid & Germany 
+                is my fav International Club & Team resp. 
+                I use to spend my free time while writing 
+                answers on Quora. Hindi songs are my go-to
+                 whenever I'm alone.</p><br></br><br></br>
+                 </div>
+            </div>
+            </div>
+            skvjkfdv
         </div>
 
     )
