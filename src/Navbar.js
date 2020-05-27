5739fb1044b886e6b22fd4124802ff11b826c4b4
anuraj07 <deep.anuraj10@gmail.com>
2020-05-27 16:28:28 +0530

    Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbarstyle.css'
+
+const Navbar = () => {
+    return (
+        <nav className = "navbarstyle">
+            <label className = "logo">Portfolio</label>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">My Work</a></li>
+                <li><a href="#">My Resume</a></li>
+                <li><a href="#">Reach Me</a></li>
+            </ul>
+        </nav>
+    )
+}
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Navbar from './Navbar'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Navbar/>
   </React.StrictMode>,
   document.getElementById('root')
 );
